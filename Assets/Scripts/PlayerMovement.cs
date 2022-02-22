a13ba9d3248a3e4ba25d7ea9115f82d13b495ca2
Negin <neginsaeidi@yahoo.com>
2022-02-22 12:47:01 -0500

    Added zombie enemy , added walking animation to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,28 +9,47 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float runSpeed = 10;
     [SerializeField]
-    private float JumpForce = 5;
+    private float JumpForce ;
 
+    [SerializeField]
+    private float gravity ;
 
-    private Rigidbody rigidbody;
+    public Vector3 velocity;
+    private CharacterController characterController;
     private Animator PlayerAnimator;
 
     Vector3 inputVector = Vector2.zero;
     Vector3 MoveDirection = Vector3.zero;
     public Vector2 lookInput = Vector2.zero;
 
+    bool isMoving = false;
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
         PlayerAnimator = GetComponent<Animator>();
     }
 
 	private void Update()
 	{
         inputVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical") ).normalized;
-        if (!(inputVector.magnitude > 0)) MoveDirection = Vector3.zero;
-        MoveDirection = transform.forward * inputVector.z + transform.right * inputVector.x;
-        Vector3 MovementDirection = MoveDirection * WalkSpeed * Time.deltaTime;
-        transform.position += MovementDirection;
+        if (!(inputVector.magnitude > 0))
+        {
+            MoveDirection = Vector3.zero;
+            isMoving = false;
+        }
+        else
+		{
+            isMoving = true;
+		}
+        PlayerAnimator.SetBool("IsMoving", isMoving);
+        velocity.y += gravity * Time.deltaTime;
+        if(Input.GetButtonDown("Jump"))
+		{
+            velocity.y = JumpForce;
+		}
+        MoveDirection = transform.forward * inputVector.z + transform.right * inputVector.x + velocity.y *Vector3.up;
+        Vector3 MovementDirection = MoveDirection * WalkSpeed * Time.deltaTime ;
+        characterController.Move(MovementDirection);
+
     }
 }
